ca4a339f3a6ee7d2368a7a773c9d2f00d989a42c
Luke A <alvidrezluke@gmail.com>
2022-07-05 13:51:31 -0500

    Added walls

--- src/physics/floor.rs
+++ src/physics/floor.rs
@@ -9,13 +9,70 @@ pub fn spawn_floor(
     commands
         .spawn_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
-            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
             ..default()
         })
-        .insert(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(Collider::cuboid(1000.0, 0.0, 100.0))
         .insert(Friction {
             coefficient: 100000000.0,
             combine_rule: CoefficientCombineRule::Multiply
         })
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)));
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 1.0, 0.0)));
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box { min_x: -50.0, max_x: 50.0, min_y: 0.0, max_y: 75.0, min_z: 0.0, max_z: 1.0})),
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+            ..default()
+        })
+        .insert(Collider::cuboid(100.0, 75.0, 1.0))
+        .insert(Friction {
+            coefficient: 100000000.0,
+            combine_rule: CoefficientCombineRule::Multiply
+        })
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 50.0)));
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box { min_x: -50.0, max_x: 50.0, min_y: 0.0, max_y: 75.0, min_z: 0.0, max_z: 1.0})),
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+            ..default()
+        })
+        .insert(Collider::cuboid(100.0, 75.0, 1.0))
+        .insert(Friction {
+            coefficient: 100000000.0,
+            combine_rule: CoefficientCombineRule::Multiply
+        })
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, -50.0)));
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box { min_x: 0.0, max_x: 1.0, min_y: 0.0, max_y: 75.0, min_z: -50.0, max_z: 50.0})),
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+            ..default()
+        })
+        .insert(Collider::cuboid(100.0, 75.0, 1.0))
+        .insert(Friction {
+            coefficient: 100000000.0,
+            combine_rule: CoefficientCombineRule::Multiply
+        })
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(50.0, 0.0, 0.0)));
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box { min_x: 0.0, max_x: 1.0, min_y: 0.0, max_y: 75.0, min_z: -50.0, max_z: 50.0})),
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+            ..default()
+        })
+        .insert(Collider::cuboid(100.0, 75.0, 1.0))
+        .insert(Friction {
+            coefficient: 100000000.0,
+            combine_rule: CoefficientCombineRule::Multiply
+        })
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(-50.0, 0.0, 0.0)));
+}
+
+pub fn spawn_light(
+    mut commands: Commands
+) {
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 1.0
+    })
 }
